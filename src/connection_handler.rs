b41d2311bfecffaaf182c31b925672b51c803c7b
Chad Baxter <cbax@doslabelectronics.com>
2021-05-23 02:39:07 -0400

    dummy test

--- src/connection_handler.rs
+++ src/connection_handler.rs
@@ -7,27 +7,19 @@ pub fn new_connection(stream: TcpStream) {
     let mut buf: Vec<u8> = vec![];
     let mut reader = BufReader::new(stream);
 
-    /*while stream. {
-        //reader.read_line(buf);
-        match reader.read_until(0x0D, &mut buf) {
-            Ok(_) => println!("{}", String::from_utf8_lossy(buf.as_slice())),
-            Err(e) => eprintln!("{}", e),
-        }
-        buf = vec![];
-    }*/
-
     loop {
+        //let mut iter_buf: [u8;16] = [0;16];
         match reader.read_until(0x0D, &mut buf) {
             Ok(o) => {
                 if o == 0 {
                     break;
                 }
-                println!("{}", String::from_utf8_lossy(buf.as_slice()));
+                println!("{}", String::from_utf8_lossy(buf.as_slice()).trim());
             },
             Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
                 // wait until network socket is ready, typically implemented
                 // via platform-specific APIs such as epoll or IOCP
-                std::thread::sleep(std::time::Duration::from_secs(1));
+                std::thread::sleep(std::time::Duration::from_millis(1));
             }
             Err(_) => break,
         };
